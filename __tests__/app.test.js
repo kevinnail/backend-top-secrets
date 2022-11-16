477e939e9f092b573c072f8f4ff165bcf2cd4fa5
kevinnail <fumalicious@gmail.com>
2022-11-16 10:43:29 -0800

    changed .env file, set up db, first test partially written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
@@ -7,9 +8,19 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  const fakeUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'super@email.com',
+    password: '12345',
+  };
+
+  it('POST api/v1/users creates a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(fakeUser);
+    expect(resp.status).toBe(200);
   });
+
   afterAll(() => {
     pool.end();
   });
