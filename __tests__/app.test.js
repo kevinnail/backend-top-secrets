0b3cc26664bc945d2c5305b7a11b50ff1cb7273d
kevinnail <fumalicious@gmail.com>
2022-11-16 16:01:35 -0800

    add user controller/ model/ userService, POST / create user test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -19,6 +19,13 @@ describe('backend-express-template routes', () => {
   it('POST api/v1/users creates a new user', async () => {
     const resp = await request(app).post('/api/v1/users').send(fakeUser);
     expect(resp.status).toBe(200);
+    const { firstName, lastName, email } = fakeUser;
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 
   afterAll(() => {
