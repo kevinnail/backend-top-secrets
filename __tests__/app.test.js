1afc399a9fe3cb82efc43efbd23f699f74f986c8
kevinnail <fumalicious@gmail.com>
2022-11-16 22:06:23 -0800

    add DELETE /sessions, test, controller, test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -28,12 +28,23 @@ describe('backend-express-template routes', () => {
     });
   });
 
-  it('POST /api/v1/users/sessions signs in an existing user', async () => {
+  it.skip('POST /api/v1/users/sessions signs in an existing user', async () => {
     await request(app).post('/api/v1/users').send(fakeUser);
     const resp = await request(app)
       .post('/api/v1/users/sessions')
       .send({ email: 'super@email.com', password: '12345' });
     expect(resp.status).toBe(200);
+  });
+
+  it('DELETE /api/v1/users/sessions deletes the user session', async () => {
+    const agent = request.agent(app);
+    const user = await UserService.create({ ...fakeUser });
+
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'super@email.com', password: '12345' });
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
   });
 
   afterAll(() => {
